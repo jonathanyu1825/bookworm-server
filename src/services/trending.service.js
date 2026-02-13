bbc3ee5d2b7f5c969bf14434bb5926e8f567dd17
jonathanyu1825 <112720627+jonathanyu1825@users.noreply.github.com>
2026-02-13 00:38:32 -0500

    cache trending

--- src/services/trending.service.js
+++ src/services/trending.service.js
@@ -1,10 +1,15 @@
 import { supabase } from "../utils/supabaseClient.js";
+import { setCache, getCache } from "../services/cacheService.js";
 
 export async function fetchTrendingBooks() {
+  const cached = await getCache("trending");
+  if (cached) {
+    return cached;
+  }
   const { data, error } = await supabase.rpc("get_trending_books");
-  console.log(data);
   if (error) {
     console.log(error);
   }
-return data;
+  setCache("trending", data);
+  return data;
 }
