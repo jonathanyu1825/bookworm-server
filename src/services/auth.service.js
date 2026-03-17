4922e853d33639eefd5a9991eb9db2aaf5a02596
Jonathan Yu <jonathanyu1825@gmail.com>
2026-03-17 00:23:46 -0400

    note

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,12 @@
 import { supabase } from "../utils/supabaseClient.js";
 
-export async function loginUserService() {
-  // const { data, error } = await supabase.from("")
+export async function loginUserService(username, password) {
+
+  // https://supabase.com/docs/reference/javascript/admin-api
+  const { data, error } = await supabase
+    .from("users")
+    .select("username, auth.users(email)")
+    .eq("username", username);
+  console.log(data);
   return { hi: "hi" };
 }
