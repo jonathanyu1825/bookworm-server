96d6045dc1cb95ae808b672152f51fdf22604c4f
jonathanyu1825 <112720627+jonathanyu1825@users.noreply.github.com>
2026-03-24 06:17:07 -0400

    rate limiting install

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 import bookRoutes from "./src/routes/books.routes.js";
 import searchRoutes from "./src/routes/search.routes.js";
 import trendingRoutes from "./src/routes/trending.routes.js";
 import authRoutes from "./src/routes/auth.routes.js";
+
 
 import { connectRedis } from "./src/config/redis.js";
 
@@ -16,7 +18,15 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
+// const limiter = rateLimit({
+//     windowMs: 15 * 60 * 1000,
+//     max: 10,
+//     message: "too many requests"
+// })
+
 app.use(express.json());
+
+// app.use(limiter);
 
 // update to only allow frontend
 app.use(cors());
@@ -29,7 +39,6 @@ app.use("/auth", authRoutes);
 app.listen(PORT, () => {
     console.log("Server is running");
 })
-
 // app.listen(PORT, '0.0.0.0', () => {
 //     console.log("Server is running");
 // })
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
+
 import { loginUser } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
+
+
 
 router.post('/login', loginUser);
 
